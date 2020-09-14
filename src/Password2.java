e8ce75f4a7f7ff49a589d29757fcd235ae018b0d
Mia <miadong179@gmail.com>
2020-09-14 17:51:40 +0200

    gabs

--- src/Password2.java
+++ src/Password2.java
@@ -3,30 +3,24 @@ import java.util.Scanner;
 public class Password2 {
 
     public static void main(String[] args) {
+        String password = "";
+        Scanner input = new Scanner(System.in);
 
-        String password = " ";
-        Scanner in = new Scanner(System.in);
+        while (password.length()<=4 || password.length()>=9) {
 
-
-
-        int length = password.length();
-
-        while (length > 9 || length < 4) {
-
-            System.out.print("Enter your password: ");
-            password = in.next();
-
-            if (length > 9) {
-                System.out.println("Password too long");
-            } else if (length < 4) {
-                System.out.println("Password too short");
+            System.out.println("Skriv password");
+            password = input.next();
+            //Input.close() skal ikke bruges da scanneren dermed skal åbnes igen hver iteration af loopet
+            if (password.length()>=9) {
+                System.out.println("password er for langt");
             }
-            System.out.print("Enter your password: ");
-
-
-
+            else if (password.length()<=4){
+                System.out.println("password er for kort");
+            }
         }
-            System.out.println("Password tilpas");
+
+        System.out.println("password er tilpas");
+        System.out.println(password);
 
 
 
