4799805218240235a235f11eee13299c3e011fd8
Mia <miadong179@gmail.com>
2020-09-19 00:13:27 +0200

    pyramide_men for mange stjerner

--- src/Pyramider.java
+++ src/Pyramider.java
@@ -4,41 +4,33 @@ public class Pyramider {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int count = 1;
-
-        /*if (n > 0) {
-            for (int i = 0; i < 3; i++) {
-                System.out.print(".");
-            }
-            System.out.print("#");
-            for (int i = 0; i < 3; i++) {
-                System.out.print(".");
-            }
-
-
-        }
-
-         */
-
+        //Scanner s = new Scanner(System.in);
+        //int n = s.nextInt();
+        int n = 7;
+        int hashtag = 0;
 
         for(int i = 0; i < n; i++){
 
-            for (int j = 0; j <= i; j++) {
-                System.out.print("#");
-
-            }
-            for (int k = n; k > i; k--) {
+            for (int k = n - 1; k > i; k--) {
                 System.out.print(".");
 
             }
 
+            hashtag = 2 * i - 1;
+            for (int j = 1; j <= hashtag; j++) {
+                System.out.print("#");
+
+            }
+
+            for (int k = n - 1; k > i; k--) {
+                System.out.print(".");
+
+            }
             System.out.print("\n");
 
         }
 
-
     }
+
 
 }
