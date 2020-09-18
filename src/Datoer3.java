c560717c38d94b7422e5b61beb9c8039d3713fbd
Mia <miadong179@gmail.com>
2020-09-18 15:09:50 +0200

    Works1

---
+++ src/Datoer3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Datoer3 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int år = 0;
+        int måned = 0;
+        int dag = 0;
+
+        System.out.println("DAGEN EFTER d..");
+        System.out.println("skriv dato, måned og år:");
+        dag = s.nextInt();
+        måned = s.nextInt();
+        år = s.nextInt();
+
+
+        if (år%4 == 0 && år%100!=0){
+
+        } else {
+
+        }
+
+    }
+
+
+
+
+}
