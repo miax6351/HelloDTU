1aaf75f07129b1edf92d7a678e788821fd4ee897
Mia <miadong179@gmail.com>
2020-09-20 18:52:12 +0200

    pyramide_VIRKER

--- src/Pyramider.java
+++ src/Pyramider.java
@@ -8,28 +8,35 @@ public class Pyramider {
         int n = s.nextInt();
         int hashtag = 0;
 
-        for(int i = 1; i < n; i++){
+        System.out.println("Pyramid " + n);
 
-            for (int k = n - 1; k > i; k--) {
-                System.out.print(".");
+        if (n == 1) {
+            System.out.println("#");
+        } else {
+
+            for (int i = 1; i <= n; i++) {
+
+                for (int k = n; k > i; k--) {
+                    System.out.print(".");
+
+                }
+
+                hashtag = 2 * i - 1;
+                for (int j = 1; j <= hashtag; j++) {
+                    System.out.print("#");
+
+                }
+
+                for (int k = n; k > i; k--) {
+                    System.out.print(".");
+
+                }
+                System.out.print("\n");
 
             }
 
-            hashtag = 2 * i - 1;
-            for (int j = 1; j <= hashtag; j++) {
-                System.out.print("#");
-
-            }
-
-            for (int k = n - 1; k > i; k--) {
-                System.out.print(".");
-
-            }
-            System.out.print("\n");
 
         }
-
+        s.close();
     }
-
-
 }
