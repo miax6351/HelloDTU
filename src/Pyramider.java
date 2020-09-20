8158608e2bcfe7117396d2c1cf9f93b7ebb8a7cf
Mia <miadong179@gmail.com>
2020-09-20 19:04:19 +0200

    pyramide_VIRKER - alle printes

--- src/Pyramider.java
+++ src/Pyramider.java
@@ -8,35 +8,46 @@ public class Pyramider {
         int n = s.nextInt();
         int hashtag = 0;
 
-        System.out.println("Pyramid " + n);
 
-        if (n == 1) {
-            System.out.println("#");
-        } else {
 
-            for (int i = 1; i <= n; i++) {
+        for (int p = 1; p <= n; p++) {
 
-                for (int k = n; k > i; k--) {
-                    System.out.print(".");
+            if (n == 1) {
+                System.out.println("Pyramid " + 1);
+                System.out.println("#");
+            } else {
+
+                System.out.println("Pyramid " + p);
+
+                for (int i = 1; i <= p; i++) {
+
+                    //der skal printes punktummer ift niveau pyramide
+                    for (int k = p; k > i; k--) {
+                        System.out.print(".");
+
+                    }
+
+                    //antallet af hashtags pr niveau pyramide
+                    hashtag = 2 * i - 1;
+                    for (int j = 1; j <= hashtag; j++) {
+                        System.out.print("#");
+
+                    }
+
+                    //der skal printes punktummer ift niveau pyramide
+                    for (int k = p; k > i; k--) {
+                        System.out.print(".");
+
+                    }
+                    //nyt niveau
+                    System.out.print("\n");
 
                 }
-
-                hashtag = 2 * i - 1;
-                for (int j = 1; j <= hashtag; j++) {
-                    System.out.print("#");
-
-                }
-
-                for (int k = n; k > i; k--) {
-                    System.out.print(".");
-
-                }
-                System.out.print("\n");
-
             }
 
 
-        }
-        s.close();
+            }
+            s.close();
+
     }
 }
